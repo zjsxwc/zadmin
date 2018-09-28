9510c70a038e9b6d7f35407283ef47757571961d
Wang Chao <watcher.hangzhou@foxmail.com>
2018-09-28 10:47:18 +0800

    translation

---
+++ src/Application/FrontBundle/Admin/UserAdmin.php
@@ -0,0 +1,189 @@
+<?php
+
+namespace Application\FrontBundle\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+class UserAdmin extends AbstractAdmin
+{
+    protected $translationDomain = 'ApplicationFrontBundle';
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('username')
+            ->add('usernameCanonical')
+            ->add('email')
+            ->add('emailCanonical')
+            ->add('enabled')
+            ->add('salt')
+            ->add('password')
+            ->add('lastLogin')
+            ->add('confirmationToken')
+            ->add('passwordRequestedAt')
+            ->add('roles')
+            ->add('createdAt')
+            ->add('updatedAt')
+            ->add('dateOfBirth')
+            ->add('firstname')
+            ->add('lastname')
+            ->add('website')
+            ->add('biography')
+            ->add('gender')
+            ->add('locale')
+            ->add('timezone')
+            ->add('phone')
+            ->add('facebookUid')
+            ->add('facebookName')
+            ->add('facebookData')
+            ->add('twitterUid')
+            ->add('twitterName')
+            ->add('twitterData')
+            ->add('gplusUid')
+            ->add('gplusName')
+            ->add('gplusData')
+            ->add('token')
+            ->add('twoStepVerificationCode')
+            ->add('id')
+            ->add('mobileNumber')
+            ->add('mobileCountry')
+        ;
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->add('username')
+            ->add('usernameCanonical')
+            ->add('email')
+            ->add('emailCanonical')
+            ->add('enabled')
+            ->add('salt')
+            ->add('password')
+            ->add('lastLogin')
+            ->add('confirmationToken')
+            ->add('passwordRequestedAt')
+            ->add('roles')
+            ->add('createdAt')
+            ->add('updatedAt')
+            ->add('dateOfBirth')
+            ->add('firstname')
+            ->add('lastname')
+            ->add('website')
+            ->add('biography')
+            ->add('gender')
+            ->add('locale')
+            ->add('timezone')
+            ->add('phone')
+            ->add('facebookUid')
+            ->add('facebookName')
+            ->add('facebookData')
+            ->add('twitterUid')
+            ->add('twitterName')
+            ->add('twitterData')
+            ->add('gplusUid')
+            ->add('gplusName')
+            ->add('gplusData')
+            ->add('token')
+            ->add('twoStepVerificationCode')
+            ->add('id')
+            ->add('mobileNumber')
+            ->add('mobileCountry')
+            ->add('_action', null, [
+                'actions' => [
+                    'show' => [],
+                    'edit' => [],
+                    'delete' => [],
+                ],
+            ])
+        ;
+    }
+
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('username')
+            ->add('usernameCanonical')
+            ->add('email')
+            ->add('emailCanonical')
+            ->add('enabled')
+            ->add('salt')
+            ->add('password')
+            ->add('lastLogin')
+            ->add('confirmationToken')
+            ->add('passwordRequestedAt')
+            ->add('roles')
+            ->add('createdAt')
+            ->add('updatedAt')
+            ->add('dateOfBirth')
+            ->add('firstname')
+            ->add('lastname')
+            ->add('website')
+            ->add('biography')
+            ->add('gender')
+            ->add('locale')
+            ->add('timezone')
+            ->add('phone')
+            ->add('facebookUid')
+            ->add('facebookName')
+            ->add('facebookData')
+            ->add('twitterUid')
+            ->add('twitterName')
+            ->add('twitterData')
+            ->add('gplusUid')
+            ->add('gplusName')
+            ->add('gplusData')
+            ->add('token')
+            ->add('twoStepVerificationCode')
+            ->add('id')
+            ->add('mobileNumber')
+            ->add('mobileCountry')
+        ;
+    }
+
+    protected function configureShowFields(ShowMapper $showMapper)
+    {
+        $showMapper
+            ->add('username')
+            ->add('usernameCanonical')
+            ->add('email')
+            ->add('emailCanonical')
+            ->add('enabled')
+            ->add('salt')
+            ->add('password')
+            ->add('lastLogin')
+            ->add('confirmationToken')
+            ->add('passwordRequestedAt')
+            ->add('roles')
+            ->add('createdAt')
+            ->add('updatedAt')
+            ->add('dateOfBirth')
+            ->add('firstname')
+            ->add('lastname')
+            ->add('website')
+            ->add('biography')
+            ->add('gender')
+            ->add('locale')
+            ->add('timezone')
+            ->add('phone')
+            ->add('facebookUid')
+            ->add('facebookName')
+            ->add('facebookData')
+            ->add('twitterUid')
+            ->add('twitterName')
+            ->add('twitterData')
+            ->add('gplusUid')
+            ->add('gplusName')
+            ->add('gplusData')
+            ->add('token')
+            ->add('twoStepVerificationCode')
+            ->add('id')
+            ->add('mobileNumber')
+            ->add('mobileCountry')
+        ;
+    }
+}
